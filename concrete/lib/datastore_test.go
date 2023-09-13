7331741f05c135e2d29de3c252219b607ca834f5
bytes <therealbyt3s@gmail.com>
2023-09-13 14:25:06 +0200

    test: datastore, slot

--- concrete/lib/datastore_test.go
+++ concrete/lib/datastore_test.go
@@ -21,6 +21,7 @@
 package lib
 
 import (
+	"math/big"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -31,16 +32,11 @@ import (
 
 func TestEnvKeyValueStore(t *testing.T) {
 	var (
-		r       = require.New(t)
-		address = common.HexToAddress("0xc0ffee0001")
-		config  = api.EnvConfig{
-			Static:    false,
-			Ephemeral: false,
-			Preimages: false,
-			Trusted:   false,
-		}
-		meterGas = true
-		gas      = uint64(1e6)
+		r        = require.New(t)
+		address  = common.HexToAddress("0xc0ffee0001")
+		config   = api.EnvConfig{Trusted: true, Ephemeral: true}
+		meterGas = false
+		gas      = uint64(0)
 	)
 	tests := []struct {
 		name string
@@ -57,9 +53,79 @@ func TestEnvKeyValueStore(t *testing.T) {
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			key := common.Hash{0x01}
-			value := test.kv.Get(key)
+			var (
+				kv  = test.kv
+				key = common.Hash{0x01}
+			)
+			value := kv.Get(key)
 			r.Equal(common.Hash{}, value)
+			kv.Set(key, common.Hash{0x02})
+			value = kv.Get(key)
+			r.Equal(common.Hash{0x02}, value)
 		})
 	}
+}
+
+func TestDatastore(t *testing.T) {
+	var (
+		r        = require.New(t)
+		address  = common.HexToAddress("0xc0ffee0001")
+		config   = api.EnvConfig{}
+		meterGas = false
+		gas      = uint64(0)
+	)
+	env := mock.NewMockEnvironment(address, config, meterGas, gas)
+	ds := NewPersistentDatastore(env)
+	key := []byte{0x01}
+	slot := ds.Get(key)
+	r.NotNil(slot)
+	r.NotNil(slot.Datastore())
+	r.Equal(slot.Slot(), common.BytesToHash(key))
+}
+
+func TestDatastoreSlot(t *testing.T) {
+	var (
+		r        = require.New(t)
+		address  = common.HexToAddress("0xc0ffee0001")
+		config   = api.EnvConfig{}
+		meterGas = false
+		gas      = uint64(0)
+	)
+	env := mock.NewMockEnvironment(address, config, meterGas, gas)
+	ds := NewPersistentDatastore(env)
+	key := []byte{0x01}
+	slot := ds.Get(key)
+
+	r.Equal(common.Hash{}, slot.Bytes32())
+	r.Equal(false, slot.Bool())
+	r.Equal(common.Address{}, slot.Address())
+	r.Equal(int64(0), slot.BigUint().Int64())
+	r.Equal(int64(0), slot.BigInt().Int64())
+	r.Equal(uint64(0), slot.Uint64())
+	r.Equal(int64(0), slot.Int64())
+	r.Equal([]byte{}, slot.Bytes())
+
+	slot.SetBytes32(common.Hash{0x01})
+	r.Equal(common.Hash{0x01}, slot.Bytes32())
+
+	slot.SetBool(true)
+	r.Equal(true, slot.Bool())
+
+	slot.SetAddress(address)
+	r.Equal(address, slot.Address())
+
+	slot.SetBigUint(big.NewInt(1))
+	r.Equal(int64(1), slot.BigUint().Int64())
+
+	slot.SetBigInt(big.NewInt(-1))
+	r.Equal(int64(-1), slot.BigInt().Int64())
+
+	slot.SetUint64(1)
+	r.Equal(uint64(1), slot.Uint64())
+
+	slot.SetInt64(-1)
+	r.Equal(int64(-1), slot.Int64())
+
+	slot.SetBytes([]byte{0x01, 0x02, 0x03})
+	r.Equal([]byte{0x01, 0x02, 0x03}, slot.Bytes())
 }
